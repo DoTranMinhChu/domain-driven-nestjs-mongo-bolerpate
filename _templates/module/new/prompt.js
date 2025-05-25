25fc42c6e150e54499b43e92bb8b19565e0d5dae
dotranminhchu <dotranminhchu@gmail.com>
2025-05-26 02:11:51 +0700

    [ADD]: Data for hygen

--- _templates/module/new/prompt.js
+++ _templates/module/new/prompt.js
@@ -1,9 +1,3 @@
-const toKebabCase = (str) =>
-  str
-    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
-    .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
-    .toLowerCase();
-
 module.exports = [
   {
     type: 'input',
@@ -26,19 +20,5 @@ module.exports = [
         selected: true,
       },
     ],
-  },
-  {
-    type: 'input',
-    name: 'computed',
-    message: '',
-    initial: '',
-    skip: true,
-    result: (_, answers) => {
-      const name = answers.name;
-      const Name = name.charAt(0).toUpperCase() + name.slice(1);
-      const kebab = toKebabCase(name);
-      const camelCase = name.charAt(0).toLowerCase() + name.slice(1);
-      return { Name, kebab, camelCase };
-    },
   },
 ];
